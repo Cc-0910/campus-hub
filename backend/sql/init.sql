5fa4368d5be86f5f35dafff68a5cbcd865d8e997
林聪聪 <15984810+lincc0910@user.noreply.gitee.com>
2025-12-27 16:22:25 +0800

    feat: add init.sql with admin data

--- backend/sql/init.sql
+++ backend/sql/init.sql
@@ -9,3 +9,6 @@ CREATE TABLE sys_user (
     create_time DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     INDEX idx_username (username)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='用户表';
+-- 插入默认管理员账号 (密码是 123456)
+INSERT INTO sys_user (username, password, nickname, email, avatar) 
+VALUES ('admin', '123456', '超级管理员', 'admin@csu.edu.cn', 'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png');
